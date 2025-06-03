54d745c7da6ad57c3571ca5a6a15bd1811c1b565
Eduardo-mEsteves <eduardofrancamesteves@gmail.com>
2025-06-03 17:51:02 -0300

    Arquivo csv e correções no codigo

--- trabalho.py
+++ trabalho.py
@@ -1,26 +1,28 @@
+import time
 import pandas as pd
 import matplotlib.pyplot as plt
+from os import system as sys; sys('cls')
 
 #inicializando o dataframe
-alunos_df = pd.DataFrame(columns=['Aluno', 'Matemática', 'Português', 'História', 'Química'])
+alunos_df = pd.DataFrame(columns=['Aluno', 'Matematica', 'Portugues', 'Historia', 'Quimica'])
 
 def adicionar_aluno():
     nome = input("Nome do aluno: ")
     try:
-        matematica = float(input("Nota de Matemática: "))
-        portugues = float(input("Nota de Português: "))
-        historia = float(input("Nota de História: "))
-        quimica = float(input("Nota de Química: "))
+        Matematica = float(input("Nota de Matemática: "))
+        Portugues = float(input("Nota de Português: "))
+        Historia = float(input("Nota de História: "))
+        Quimica = float(input("Nota de Química: "))
     except ValueError:
         print("Digite um nome válido.")
         return
     
     novo = pd.DataFrame([{
         'Aluno': nome,
-        'Matemática': matematica,
-        'Português': portugues,
-        'História': historia,
-        'Química': quimica
+        'Matematica': Matematica,
+        'Portugues': Portugues,
+        'Historia': Historia,
+        'Quimica': Quimica
     }])
     global alunos_df
     alunos_df = pd.concat([alunos_df, novo], ignore_index=True)
@@ -38,7 +40,7 @@ def mostrar_por_materia():
 def mostrar_geral():
     #Feito por gabriel, mas deu problema no merge
     df = alunos_df.copy()
-    df['Média Geral'] = df[['Matemática', 'Português', 'Historia', 'Química']].mean(axis=1)
+    df['Média Geral'] = df[['Matematica', 'Portugues', 'Historia', 'Quimica']].mean(axis=1)
     ranking= df.sort_values(by='Média Geral', ascending=False)
     print("\nRanking - Média Geral")
     print(ranking[['Aluno', 'Média Geral']])
@@ -56,7 +58,7 @@ def gerar_grafico_materias():
 
 def gerar_grafico_geral():
     df = alunos_df.copy() 
-    df[['Matematica', 'Portugues', 'Historia', 'Quimica']].mean(axis=1) 
+    df['Média Geral'] = df[['Matematica', 'Portugues', 'Historia', 'Quimica']].mean(axis=1) 
     df = df.set_index('Aluno') 
     df['Média Geral'].sort_values(ascending=False).plot(kind='bar', color='lightgreen', figsize=(10, 5)) 
     plt.title('Ranking - Média Geral dos Alunos')
@@ -74,6 +76,7 @@ def salvar_dados():
     
 
 while True:
+    print("\nSistema de Ranking de Médias")
     print("1-Adicionar novo aluno")
     print("2-Ver ranking por materia")
     print("3-Ver ranking geral")
@@ -81,19 +84,34 @@ while True:
     print("5-Gerar gráfico de média geral")
     print("6-Salvar dados")
     print("7-Sair")
+
     opcao = input("Escolha uma opção: ")
+
     if opcao == '1':
         adicionar_aluno()
+        time.sleep(3)
+        sys('cls')
     elif opcao == '2':
         mostrar_por_materia()
+        time.sleep(3)
+        print("")
     elif opcao == '3':
         mostrar_geral()
+        time.sleep(3)
+        print("")
+
     elif opcao == '4':
         gerar_grafico_materias()
+        time.sleep(3)
+        print("")
     elif opcao == '5':
         gerar_grafico_geral()
+        time.sleep(3)
+        print("")
     elif opcao == '6':
         salvar_dados()
+        time.sleep(3)
+        print("")
     elif opcao == '7':
         print("Saindo...")
         break
